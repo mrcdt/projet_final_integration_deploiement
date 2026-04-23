b69fb261fb927880560e9815c1d7d65c8228b62d
marineCadet <marinecadet05@gmail.com>
2026-04-23 12:19:53 +0200

    Nettoyage: ajout du gitignore et suppression de node_modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const { Pool } = require('pg');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
